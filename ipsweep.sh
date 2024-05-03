b4472cd7884a0c91d92dfc5f47f62ca1e81c4d0c
Mohamed Ahmed Hassan <149105951+7amo127@users.noreply.github.com>
2024-05-03 01:38:22 -0500

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
 
+#! /bin/bash
 
 
+#!/bin/bash
 
-for ip in $(seq 1 254) ; do
-    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <network>"
+    echo "Example: $0 192.168.1"
+    exit 1
+fi
+
+network=$1
+
+for ip in $(seq 1 254); do
+    ping -c 1 $network.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
